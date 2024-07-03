ab81641c866b37298e2188a89279a33c648bb669
itzrizvi <Shahriar.me.work@gmail.com>
2024-07-04 01:00:44 +0600

    Comment added

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 import app from "./app";
 import connectDB from "./config/db";
 
+// Env Config
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// Listen Server App
 app.listen(PORT, () => {
   console.log(`🚀 Server running on PORT ${PORT}`);
 });
+
+// Connect Database
 connectDB();

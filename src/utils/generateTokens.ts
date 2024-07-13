d309e4dbbd47b3e38fd8cea6e28007155bb57928
itzrizvi <Shahriar.me.work@gmail.com>
2024-07-14 02:01:07 +0600

    Generate token function modified

--- src/utils/generateTokens.ts
+++ src/utils/generateTokens.ts
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 
-export const generateAccessToken = (id: Types.ObjectId) => {
-  return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET as string, {
+export const generateAccessToken = (_id: Types.ObjectId) => {
+  return jwt.sign({ _id }, process.env.ACCESS_TOKEN_SECRET as string, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY
   });
 };
 
-export const generateRefreshToken = (id: Types.ObjectId) => {
-  return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET as string, {
+export const generateRefreshToken = (_id: Types.ObjectId) => {
+  return jwt.sign({ _id }, process.env.REFRESH_TOKEN_SECRET as string, {
     expiresIn: process.env.REFRESH_TOKEN_EXPIRY
   });
 };
